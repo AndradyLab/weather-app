5343cb80c7e7f9764d926781a989d845ad733cef
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2026-01-29 09:48:11 -0300

    feature/config-themes-colors: set initial config to themes and colors. (#1)

---
+++ lib/ui/core/themes/colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const black1 = Color(0xFF1a1a16);
+  static const white1 = Color(0xFFFAFAFA);
+
+  static final lightColorScheme = ColorScheme(
+    brightness: Brightness.light,
+    primary: AppColors.black1,
+    secondary: Colors.white,
+    surface: Colors.white30,
+    onPrimary: Colors.white70,
+    onSecondary: AppColors.black1,
+    onSurface: AppColors.black1,
+    error: Colors.red,
+    onError: Colors.white30
+  );
+  
+  static final darkColorScheme = ColorScheme(
+    brightness: Brightness.dark,
+    primary: AppColors.white1,
+    secondary: Colors.grey,
+    surface: Colors.black38,
+    onPrimary: Colors.black38,
+    onSecondary: AppColors.white1,
+    onSurface: AppColors.white1,
+    error: Colors.red,
+    onError: Colors.white30
+  );
+  
+  
+}
